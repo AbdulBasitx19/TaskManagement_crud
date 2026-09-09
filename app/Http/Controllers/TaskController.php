a7ec4e28b9e9de401265a87c1892961327984cb5
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-09 17:25:21 +0500

    feat: create Task model, migration file, Controller and index route

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Task;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+    public function index()
+    {
+        $tasks = Task::latest()->get();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    
+}
