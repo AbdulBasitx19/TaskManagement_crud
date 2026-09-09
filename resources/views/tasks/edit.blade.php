5f777b629f166a57449842fff39cebde2fa04c83
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-09 18:33:23 +0500

    feat: set create and edit view to create or edit selected  task

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,24 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add Task</title>
+    <title>Edit task</title>
 </head>
 <body>
-    <h1>Add new Task</h1>
+    <h1>Edit Task</h1>
 
-    <form action=" {{ route('tasks.store') }}" method="POST">
-        @csrf 
+    <form action="{{ route('tasks.update', $task->id) }}" method="POST">
+        @csrf
+        @method('PUT')
 
         <label>Title: </label>
-        <input type="text" name="title" class="title" value="{{ old('title') }}">
+        <input type="text" name="title" class="title" value="{{ old('title', $task->title) }}">
          @error('title') <span style="color:red;">{{ $message }}</span> @enderror
         <br><br>
 
-        <label>Descrition: </label>
-        <textarea name="description">{{ old('description')}}</textarea>
+        <label>Description: </label>
+        <textarea name="description">{{ old('description', $task->description)}}</textarea>
         <br><br>
 
-        <button type="submit">Save</button>
+        <button type="submit">Update task</button>
         <a href="{{ route('tasks.index')}}">Cancel</a>
     </form>
     
